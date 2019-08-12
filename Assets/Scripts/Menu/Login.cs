64ce8e6caad41c1a56152adee809f94a5373bd10
irlartboy <ollie_king99@outlook.com>
2019-08-12 16:02:43 +1000

    update

---
+++ Assets/Scripts/Menu/Login.cs
@@ -0,0 +1,63 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.Networking;
+using UnityEngine.SceneManagement;
+
+
+public class Login : MonoBehaviour
+{
+    public InputField username;
+    public InputField email;
+    public InputField password;
+
+    public InputField sIUsername;
+    public InputField sIPassword;
+    public Text sInotification;
+
+    public Text cAnotification;
+
+    
+    IEnumerator CreateUser(string username, string email, string password)
+    {
+        string createUserURL = "http://localhost/nsirpg/InsertUser.php";
+        WWWForm form = new WWWForm();
+        form.AddField("username", username);
+        form.AddField("email", email);
+        form.AddField("password", password);
+        UnityWebRequest webRequest = UnityWebRequest.Post(createUserURL, form);
+        yield return webRequest.SendWebRequest();
+        cAnotification.text = webRequest.downloadHandler.text;
+
+      
+    }
+    IEnumerator SignIn(string username, string password)
+    {
+        string loginURL = "http://localhost/nsirpg/Login.php";
+        WWWForm form = new WWWForm();
+        form.AddField("username", username);
+        form.AddField("password", password);
+        UnityWebRequest webRequest = UnityWebRequest.Post(loginURL, form);
+        yield return webRequest.SendWebRequest();
+        sInotification.text = webRequest.downloadHandler.text;
+        Debug.Log(webRequest.downloadHandler.text);
+        if (webRequest.downloadHandler.text == "Successful")
+        {
+            SceneManager.LoadScene(sceneBuildIndex: 1);
+        }
+    }
+    public void CreateNewUser()
+    {
+        cAnotification.text = "";
+       StartCoroutine(CreateUser(username.text, email.text, password.text));
+    }
+
+    public void SignIn()
+    {
+        sInotification.text = "";
+        StartCoroutine(SignIn(sIUsername.text, sIPassword.text));
+      
+    }
+
+}
---
+++ Assets/Scripts/Menu/MenuManager.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class MenuManager : MonoBehaviour
+{
+
+    public GameObject SignInPanel;
+    public GameObject CreatePanel;
+    public GameObject ForgotPanel;
+  
+    public void CreateClick()
+    {
+        SignInPanel.SetActive(false);
+        CreatePanel.SetActive(true);
+    }
+    public void ForgotClick()
+    {
+        SignInPanel.SetActive(false);
+        ForgotPanel.SetActive(true);
+    }
+    public void CreateBack()
+    {
+        
+        CreatePanel.SetActive(false);
+        SignInPanel.SetActive(true);
+    }
+    public void ForgotBack()
+    {
+
+        ForgotPanel.SetActive(false);
+        SignInPanel.SetActive(true);
+    }
+
+}
